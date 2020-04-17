ee9e08fd794c09fb6482925354f0aa1b52cff0f8
NooraKalpio <noora.kalpio@gmail.com>
2020-04-17 14:24:17 +0300

    morgan got me like: WTF

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+var morgan = require('morgan')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 let persons = [
     {
         name: "Arto Hellas",
